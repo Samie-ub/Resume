afe8908fb304ef1dd8bb05b9d797864670045d47
Samie-ub <xxxsamie39@gmail.com>
2023-11-28 05:47:17 +0500

    tilt styles

--- src/app/component/PostCard.jsx
+++ src/app/component/PostCard.jsx
@@ -1,18 +1,24 @@
 "use client"
-import React, { useEffect } from "react";
+
+import React, { useEffect, useState } from "react";
 import styles from "../styles/card.module.css";
 import { Grid } from "@mui/material";
 import { galleryData } from "../content";
 
 function PostCard() {
+  const [initialGamma, setInitialGamma] = useState(null);
+
   useEffect(() => {
     const handleOrientation = (event) => {
-      const { beta, gamma } = event;
+      if (initialGamma === null) {
+        setInitialGamma(event.gamma || 0);
+      }
+
       const gyroElements = document.querySelectorAll(".gyro-element");
 
       gyroElements.forEach((element) => {
-        const tiltX = gamma || 0; // Tilt left-to-right (in degrees)
-        const tiltY = beta || 0; // Tilt front-to-back (in degrees)
+        const tiltX = (event.gamma || 0) - initialGamma; // Adjusted tilt left-to-right (in degrees)
+        const tiltY = event.beta || 0; // Tilt front-to-back (in degrees)
 
         // Apply the gyro effect to each image element
         element.style.transform = `rotateX(${tiltY}deg) rotateY(${tiltX}deg)`;
@@ -24,13 +30,13 @@ function PostCard() {
     return () => {
       window.removeEventListener("deviceorientation", handleOrientation);
     };
-  }, []);
+  }, [initialGamma]);
 
   return (
     <div>
       <Grid container justifyContent={"center"} gap={2} mt={10}>
         {galleryData.map((images, index) => (
-          <Grid item lg={3.5} key={index}>
+          <Grid item xs={12} lg={3.5} key={index}>
             <div className={`${styles.cardContainer} gyro-element`}>
               <div className={styles.cardImgContainer}>
                 <img src={images.src} alt={`image-${index}`} />
